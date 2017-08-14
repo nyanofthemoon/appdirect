6bc9756643b4a53bfc52657f1b4fb62682d6e399
Paule Lepage <paule.lepage@gmail.com>
2017-08-14 13:08:38 +0000

    Moved static files to public to protect config.json!

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     'app': './src/app.js'
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'public', 'dist'),
     filename: '[name].bundle.js'
   },
   module: {
