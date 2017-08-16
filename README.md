6611039f17f36c80cee096c27aa7d88d001fd712
Nyan Of The Moon <nyanofthemoon@gmail.com>
2017-08-16 10:28:03 -0400

    setup instructions

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ Install the project dependencies:
 
 `npm install`
 
+## Configuration
+
+`npm run setup`
+Add your twitter key and secret into `config.json`
+
 ## Running
 
 Starts the static and proxy servers:
--- src/tweets.js
+++ src/tweets.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'https://appdirect-plepage.c9users.io:8081/1.1/statuses/user_timeline.json?count=30&screen_name=';
+const API_URL = 'http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=';
 
 export default function getTweets(name) {
     const url = API_URL + encodeURIComponent(name);
